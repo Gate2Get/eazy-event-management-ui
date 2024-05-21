d2928ee0d108be2a840af9109fb0b3ea064d679c
Saravanan <saravananslb2015@gmail.com>
2024-05-21 22:09:06 +0530

    Audio mp3 fix

--- src/components/audioRecorder/index.tsx
+++ src/components/audioRecorder/index.tsx
@@ -5,6 +5,7 @@ import {
 } from "react-audio-voice-recorder";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { Button, Space } from "antd";
+import lamejs from "@breezystack/lamejs";
 
 type AudioRecorderType = {
   onUpload?: (blob: Blob) => void;
@@ -17,18 +18,63 @@ export const AudioRecorder = (props: AudioRecorderType) => {
 
   const recorderControls = useAudioRecorder();
 
-  const addAudioElement = (blob: Blob) => {
-    const url = URL.createObjectURL(blob);
-    console.log({ url, blob });
-    setUrl(url);
-    setBlob(blob);
+  const decodeAudioData = (arrayBuffer: ArrayBuffer): Promise<AudioBuffer> => {
+    return new Promise((resolve, reject) => {
+      const audioContext = new (window.AudioContext ||
+        (window as any).webkitAudioContext)();
+      audioContext.decodeAudioData(arrayBuffer, resolve, reject);
+    });
+  };
+
+  const floatTo16BitPCM = (input: Float32Array): Int16Array => {
+    const output = new Int16Array(input.length);
+    for (let i = 0; i < input.length; i++) {
+      const s = Math.max(-1, Math.min(1, input[i]));
+      output[i] = s < 0 ? s * 0x8000 : s * 0x7fff;
+    }
+    return output;
+  };
+
+  const encodeMP3 = (audioBuffer: AudioBuffer): Uint8Array[] => {
+    console.log({ audioBuffer });
+    const samples = audioBuffer.getChannelData(0);
+    const samples16Bit = floatTo16BitPCM(samples); // Convert Float32Array to Int16Array
+    const mp3Encoder = new lamejs.Mp3Encoder(1, audioBuffer.sampleRate, 256);
+    const mp3Data: Uint8Array[] = [];
+    const sampleBlockSize = 1152;
+
+    for (let i = 0; i < samples16Bit.length; i += sampleBlockSize) {
+      const sampleChunk = samples16Bit.subarray(i, i + sampleBlockSize);
+      const mp3buf = mp3Encoder.encodeBuffer(sampleChunk);
+      if (mp3buf.length > 0) {
+        mp3Data.push(mp3buf);
+      }
+    }
+
+    const mp3buf = mp3Encoder.flush();
+    if (mp3buf.length > 0) {
+      mp3Data.push(mp3buf);
+    }
+
+    return mp3Data;
+  };
+
+  const onAudio = async (audioBlob: Blob) => {
+    const arrayBuffer = await audioBlob.arrayBuffer();
+    const audioBuffer = await decodeAudioData(arrayBuffer);
+    const mp3Buffer = encodeMP3(audioBuffer);
+    const mp3Blob = new Blob(mp3Buffer, { type: "audio/mp3" });
+    const mp3Url = URL.createObjectURL(mp3Blob);
+    console.log({ mp3Url });
+    setUrl(mp3Url);
+    setBlob(mp3Blob);
   };
 
   return (
     <Space direction="vertical" style={{ width: "100%" }}>
       <div>
         <ReactAudioRecorder
-          onRecordingComplete={(blob) => addAudioElement(blob)}
+          onRecordingComplete={(blob) => onAudio(blob)}
           recorderControls={recorderControls}
           downloadFileExtension="mp3"
           showVisualizer
